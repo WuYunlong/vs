01c7a0cefd8813920bc7c7224cdeda3dadfd471a
乌云龙 <17771258@qq.com>
2022-11-30 09:13:02 +0800

    调整install到独立步骤

---
+++ scripts/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "${0}")/.."
+APP_ROOT=$(pwd)
+
+function main() {
+  cd ${APP_ROOT}
+
+  cd vscode
+	# 删除文件
+	rm -rf extensions/theme-abyss
+	rm -rf extensions/theme-kimbie-dark
+	rm -rf extensions/theme-monokai
+	rm -rf extensions/theme-monokai-dimmed
+	rm -rf extensions/theme-quietlight
+	rm -rf extensions/theme-red
+	rm -rf extensions/theme-seti
+	rm -rf extensions/theme-solarized-dark
+	rm -rf extensions/theme-solarized-light
+	rm -rf extensions/theme-tomorrow-night-blue
+
+	yarn --frozen-lockfile
+}
+
+main "$@"
